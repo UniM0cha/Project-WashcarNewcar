62b190f1f410de154f13f67db3bc45811cb5a6d8
UniM0cha <solst_ice@naver.com>
2022-08-10 15:08:33 +0900

    0810 - 상단바 만들다가 너무 어려워서 책 먼저 따라하기로 함

---
+++ washcar-newcar/src/components/Header.js
@@ -0,0 +1,17 @@
+import '../App.css';
+
+function Header() {
+  return (
+    <div>
+      <header className="top-navigation">
+        <img src="logo192.png" className="logo" alt="logo" />
+        세차새차
+        <button>햄버거</button>
+      </header>
+
+      <body></body>
+    </div>
+  );
+}
+
+export default Header;
