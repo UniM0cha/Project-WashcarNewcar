df191752449095b1e5ac91bd3dbaa42606514035
UniM0cha <solst_ice@naver.com>
2022-08-12 22:30:25 +0900

    0812 - 헤더 그림자 살짝 수정

--- washcar-newcar/src/pages/Reservation.js
+++ washcar-newcar/src/pages/Reservation.js
@@ -1,7 +1,6 @@
 import Body from '../components/Body';
 import Burgermenu from '../components/Burgermenu';
 import Header from '../components/Header';
-import MainBody from '../components/MainBody';
 
 const Reservation = () => {
   return (
