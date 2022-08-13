df0ad46a566df83ecb31e23c3c442e65e9c7cfbb
UniM0cha <solst_ice@naver.com>
2022-08-13 15:17:07 +0900

    0813 - 백엔드 프로젝트 생성

---
+++ washcar_newcar/src/main/java/com/solstice/washcar_newcar/WashcarNewcarApplication.java
@@ -0,0 +1,13 @@
+package com.solstice.washcar_newcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashcarNewcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashcarNewcarApplication.class, args);
+	}
+
+}
---
+++ washcar_newcar/src/test/java/com/solstice/washcar_newcar/WashcarNewcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solstice.washcar_newcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashcarNewcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
